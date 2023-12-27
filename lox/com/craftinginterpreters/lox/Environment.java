543b5e599516d78af2388a928ff528cd2f299d2d
hanichris <hanichris71@gmail.com>
2023-12-27 21:18:05 +0300

    Add functionality to support assignment operations. Similar to variable declarations but without the implicit creation of variables that did not exist prior.
    Signed-off-by: hanichris <hanichris71@gmail.com>

--- lox/com/craftinginterpreters/lox/Environment.java
+++ lox/com/craftinginterpreters/lox/Environment.java
@@ -17,6 +17,21 @@ class Environment {
         );
     }
 
+    /**
+     * Not allowed to create a new variable unlike the 'define' method.
+     * throws a RuntimeError if the key doesn't already exist in
+     * the environment's variable map
+     */
+    void assign(Token name, Object value) {
+        if (values.containsKey(name.lexeme)) {
+            values.put(name.lexeme, value);
+            return;
+        }
+
+        throw new RuntimeError(name,
+                "Undefined variable '" + name.lexeme + "'.");
+    }
+
     void define(String name, Object value) {
         values.put(name, value);
     }
