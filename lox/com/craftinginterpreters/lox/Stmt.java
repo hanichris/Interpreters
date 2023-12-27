de04baf43f5d3359b01e14c0dc3884772dfb8cd5
hanichris <hanichris71@gmail.com>
2023-12-27 21:20:00 +0300

    Clean up unnecessary whitespace.
    Signed-off-by: hanichris <hanichris71@gmail.com>

--- lox/com/craftinginterpreters/lox/Stmt.java
+++ lox/com/craftinginterpreters/lox/Stmt.java
@@ -5,14 +5,11 @@ import java.util.List;
 abstract class Stmt {
   interface Visitor<R> {
     R visitExpressionStmt(Expression stmt);
-
     R visitPrintStmt(Print stmt);
-
     R visitVarStmt(Var stmt);
   }
 
   abstract <R> R accept(Visitor<R> visitor);
-
   static class Expression extends Stmt {
 
     final Expr expression;
@@ -26,7 +23,6 @@ abstract class Stmt {
       return visitor.visitExpressionStmt(this);
     }
   }
-
   static class Print extends Stmt {
 
     final Expr expression;
@@ -40,7 +36,6 @@ abstract class Stmt {
       return visitor.visitPrintStmt(this);
     }
   }
-
   static class Var extends Stmt {
 
     final Token name;
