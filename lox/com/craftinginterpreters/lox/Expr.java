79b60ea152c2640febe9903d10034b898651e653
hanichris <hanichris71@gmail.com>
2023-12-27 21:17:01 +0300

    Produce the assignment expression subtree node.
    Signed-off-by: hanichris <hanichris71@gmail.com>

--- lox/com/craftinginterpreters/lox/Expr.java
+++ lox/com/craftinginterpreters/lox/Expr.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 abstract class Expr {
     interface Visitor<R> {
+        R visitAssignExpr(Assign expr);
+
         R visitBinaryExpr(Binary expr);
 
         R visitGroupingExpr(Grouping expr);
@@ -16,6 +18,22 @@ abstract class Expr {
     }
 
     abstract <R> R accept(Visitor<R> visitor);
+
+    static class Assign extends Expr {
+
+        final Token name;
+        final Expr value;
+
+        Assign(Token name, Expr value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitAssignExpr(this);
+        }
+    }
 
     static class Binary extends Expr {
 
