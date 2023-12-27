a4727c8734e1f2947821ff609bc884ec1a7b095e
hanichris <hanichris71@gmail.com>
2023-12-27 18:12:19 +0300

    Functionality to create a new statement node to support variable declarations.
    Signed-off-by: hanichris <hanichris71@gmail.com>

--- lox/com/craftinginterpreters/lox/Stmt.java
+++ lox/com/craftinginterpreters/lox/Stmt.java
@@ -3,39 +3,57 @@ package com.craftinginterpreters.lox;
 import java.util.List;
 
 abstract class Stmt {
-    interface Visitor<R> {
-        R visitExpressionStmt(Expression stmt);
+  interface Visitor<R> {
+    R visitExpressionStmt(Expression stmt);
 
-        R visitPrintStmt(Print stmt);
+    R visitPrintStmt(Print stmt);
+
+    R visitVarStmt(Var stmt);
+  }
+
+  abstract <R> R accept(Visitor<R> visitor);
+
+  static class Expression extends Stmt {
+
+    final Expr expression;
+
+    Expression(Expr expression) {
+      this.expression = expression;
     }
 
-    abstract <R> R accept(Visitor<R> visitor);
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitExpressionStmt(this);
+    }
+  }
 
-    static class Expression extends Stmt {
+  static class Print extends Stmt {
 
-        final Expr expression;
+    final Expr expression;
 
-        Expression(Expr expression) {
-            this.expression = expression;
-        }
-
-        @Override
-        <R> R accept(Visitor<R> visitor) {
-            return visitor.visitExpressionStmt(this);
-        }
+    Print(Expr expression) {
+      this.expression = expression;
     }
 
-    static class Print extends Stmt {
-
-        final Expr expression;
-
-        Print(Expr expression) {
-            this.expression = expression;
-        }
-
-        @Override
-        <R> R accept(Visitor<R> visitor) {
-            return visitor.visitPrintStmt(this);
-        }
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitPrintStmt(this);
     }
+  }
+
+  static class Var extends Stmt {
+
+    final Token name;
+    final Expr initializer;
+
+    Var(Token name, Expr initializer) {
+      this.name = name;
+      this.initializer = initializer;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitVarStmt(this);
+    }
+  }
 }
