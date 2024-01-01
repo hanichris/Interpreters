46dc70c2603b318cba99e7ce5edd4e422336050c
hanichris <hanichris71@gmail.com>
2024-01-02 01:05:16 +0300

    Support creation of a local scope and perform variable retrieval and creation within scopes.
    Signed-off-by: hanichris <hanichris71@gmail.com>

--- lox/com/craftinginterpreters/lox/Environment.java
+++ lox/com/craftinginterpreters/lox/Environment.java
@@ -4,20 +4,42 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Environment {
+    final Environment enclosing; // Reference to the enclosing environment.
     private final Map<String, Object> values = new HashMap<>();
 
     /**
-     * takes a 'Token' instead of a string to help report to the
+     * no-argument constructor for the global scope's environment which ends
+     * the chain.
+     */
+    Environment() {
+        enclosing = null;
+    }
+
+    /**
+     * creates a new local scope nested inside the given outer one.
+     */
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
+
+    /**
+     * Performs a variable lookup within the current innermost scope.
+     * If it is not found, then search the enclosing scope for the variable
+     * and recursively repeat until the global scope whereby a runtime error
+     * is thrown if the variable was not found.
+     * Takes a 'Token' instead of a string to help report to the
      * user where the in their code they messed up.
      */
     Object get(Token name) {
         if (values.containsKey(name.lexeme)) return values.get(name.lexeme);
+        if (enclosing != null) return enclosing.get(name);
         throw new RuntimeError(name,
                 "Undefined variable '" + name.lexeme + "'."
         );
     }
 
     /**
+     * Support assignment in nested environments.
      * Not allowed to create a new variable unlike the 'define' method.
      * throws a RuntimeError if the key doesn't already exist in
      * the environment's variable map
@@ -28,10 +50,18 @@ class Environment {
             return;
         }
 
+        if (enclosing != null) {
+            enclosing.assign(name, value);
+            return;
+        }
+
         throw new RuntimeError(name,
                 "Undefined variable '" + name.lexeme + "'.");
     }
 
+    /**
+     * Declares a new variable in the current innermost scope.
+     */
     void define(String name, Object value) {
         values.put(name, value);
     }
