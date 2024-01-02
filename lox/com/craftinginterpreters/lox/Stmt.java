6f7978887f8ed2b2dfe337eae9118349d483e376
hanichris <hanichris71@gmail.com>
2024-01-02 22:09:30 +0300

    Add support for the `if` syntax tree node.
    Signed-off-by: hanichris <hanichris71@gmail.com>

--- lox/com/craftinginterpreters/lox/Stmt.java
+++ lox/com/craftinginterpreters/lox/Stmt.java
@@ -5,37 +5,56 @@ import java.util.List;
 abstract class Stmt {
   interface Visitor<R> {
     R visitBlockStmt(Block stmt);
+
+      R visitIfStmt(If stmt);
     R visitExpressionStmt(Expression stmt);
     R visitPrintStmt(Print stmt);
     R visitVarStmt(Var stmt);
   }
 
   abstract <R> R accept(Visitor<R> visitor);
-
   static class Block extends Stmt {
 
     final List<Stmt> statements;
 
     Block(List<Stmt> statements) {
-      this.statements = statements;
+        this.statements = statements;
     }
 
     @Override
     <R> R accept(Visitor<R> visitor) {
-      return visitor.visitBlockStmt(this);
+        return visitor.visitBlockStmt(this);
     }
   }
+
+    static class If extends Stmt {
+
+        final Expr condition;
+        final Stmt thenBranch;
+        final Stmt elseBranch;
+
+        If(Expr condition, Stmt thenBranch, Stmt elseBranch) {
+            this.condition = condition;
+            this.thenBranch = thenBranch;
+            this.elseBranch = elseBranch;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitIfStmt(this);
+        }
+    }
   static class Expression extends Stmt {
 
     final Expr expression;
 
     Expression(Expr expression) {
-      this.expression = expression;
+        this.expression = expression;
     }
 
     @Override
     <R> R accept(Visitor<R> visitor) {
-      return visitor.visitExpressionStmt(this);
+        return visitor.visitExpressionStmt(this);
     }
   }
   static class Print extends Stmt {
@@ -43,12 +62,12 @@ abstract class Stmt {
     final Expr expression;
 
     Print(Expr expression) {
-      this.expression = expression;
+        this.expression = expression;
     }
 
     @Override
     <R> R accept(Visitor<R> visitor) {
-      return visitor.visitPrintStmt(this);
+        return visitor.visitPrintStmt(this);
     }
   }
   static class Var extends Stmt {
@@ -57,13 +76,13 @@ abstract class Stmt {
     final Expr initializer;
 
     Var(Token name, Expr initializer) {
-      this.name = name;
-      this.initializer = initializer;
+        this.name = name;
+        this.initializer = initializer;
     }
 
     @Override
     <R> R accept(Visitor<R> visitor) {
-      return visitor.visitVarStmt(this);
+        return visitor.visitVarStmt(this);
     }
   }
 }
