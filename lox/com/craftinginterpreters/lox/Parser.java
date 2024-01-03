772dbc745207a043b7ce54db9d746696b49eb5af
hanichris <hanichris71@gmail.com>
2024-01-03 22:28:51 +0300

    Parse a while statement within a script or in the REPL.
    Signed-off-by: hanichris <hanichris71@gmail.com>

--- lox/com/craftinginterpreters/lox/Parser.java
+++ lox/com/craftinginterpreters/lox/Parser.java
@@ -45,6 +45,7 @@ class Parser {
     private Stmt statement() {
         if (match(TokenType.IF)) return ifStatement();
         if (match(TokenType.PRINT)) return printStatement();
+        if (match(TokenType.WHILE)) return whileStatement();
         if (match(TokenType.LEFT_BRACE)) return new Stmt.Block(block());
         return expressionStatement();
     }
@@ -84,6 +85,15 @@ class Parser {
         if (match(TokenType.EQUAL)) initializer = expression();
         consume(TokenType.SEMICOLON, "Expect a ';' after a variable declaration");
         return new Stmt.Var(name, initializer);
+    }
+
+    private Stmt whileStatement() {
+        consume(TokenType.LEFT_PAREN, "Expect '(' after while");
+        Expr condition = expression();
+        consume(TokenType.RIGHT_PAREN, "Expect ')' after condition");
+        Stmt body = statement();
+
+        return new Stmt.While(condition, body);
     }
 
     /**
