03eaf23d4bb13bebfe95782711ed89fbc7cac6eb
hanichris <hanichris71@gmail.com>
2024-01-03 22:28:27 +0300

    Process while statements.
    Signed-off-by: hanichris <hanichris71@gmail.com>

--- lox/com/craftinginterpreters/lox/Interpreter.java
+++ lox/com/craftinginterpreters/lox/Interpreter.java
@@ -189,6 +189,12 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         return null;
     }
 
+    @Override
+    public Void visitWhileStmt(Stmt.While stmt) {
+        while (isTruthy(evaluate(stmt.condition))) execute(stmt.body);
+        return null;
+    }
+
     /**
      * returns the assigned value since assignment expressions can be nested
      * within other expressions.
