6bc43d242c3144d499689cfc7d735468189cedc2
hanichris <hanichris71@gmail.com>
2024-01-14 22:12:59 +0300

    Use the try-catch block to unwind a return statement deeply nested within other statements. If not explicit return statement is issued, simply return nil from the function.
    Signed-off-by: hanichris <hanichris71@gmail.com>

--- lox/com/craftinginterpreters/lox/LoxFunction.java
+++ lox/com/craftinginterpreters/lox/LoxFunction.java
@@ -20,7 +20,15 @@ class LoxFunction implements LoxCallable {
             );
         }
 
-        interpreter.executeBlock(declaration.body, environment);
+        // Call the executeBlock in a try-catch block. If it catches a return
+        // exception, it pulls out the value and makes the return value from the call.
+        // If it never catches one of those exceptions, it implies the function reaches
+        // the end without hitting a return statement and thus implicitly returns nil.
+        try {
+            interpreter.executeBlock(declaration.body, environment);
+        } catch (Return returnValue) {
+            return returnValue.value;
+        }
         return null;
     }
 
