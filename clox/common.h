ddc37fa3df774bed665777595bcc077f2d9a46cb
hanichris <hanichris71@gmail.com>
2024-01-17 23:12:55 +0300

    Header file for the main program.
    Defines required standard library files.

---
+++ clox/common.h
@@ -0,0 +1,8 @@
+#ifndef clox_common_h
+#define clox_common_h
+
+#include <stdbool.h> // defines boolean types for C99
+#include <stddef.h> // defining NULL and size_t
+#include <stdint.h> // for explicitly-sized integer types e.g uint8_t
+
+#endif
