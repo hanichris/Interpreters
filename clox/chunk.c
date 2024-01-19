a68a8b549045e73657bc8c55ee959349b1399726
hanichris <hanichris71@gmail.com>
2024-01-19 22:33:51 +0300

    Provide function documentation to the functions `initChunk` and `writeChunk`

--- clox/chunk.c
+++ clox/chunk.c
@@ -2,20 +2,37 @@
 #include "chunk.h"
 #include "memory.h"
 
-void initChunk(Chunk* chunk) {
-    chunk->count = 0;
-    chunk->capacity = 0;
-    chunk->count = NULL;
+
+/**
+ * initChunk - initialize a dynamic array with some defaults.
+ * @chunk: pointer to a struct defining a dynamic array.
+ * Return: void
+*/
+void initChunk(Chunk *chunk)
+{
+	chunk->count = 0;
+	chunk->capacity = 0;
+	chunk->count = NULL;
 }
 
-void writeChunk(Chunk* chunk, uint8_t byte) {
-    if (chunk->capacity < chunk->count + 1) {
-        int oldCapacity = chunk->capacity;
-        chunk->capacity = GROW_CAPACITY(oldCapacity);
-        chunk->code = GROW_ARRAY(u_int8_t, chunk->code, oldCapacity, chunk->capacity);
-    }
+/**
+ * writeChunk - Append a new byte to the end of a dynamic array.
+ * @chunk: pointer to a struct defining a dynamic array.
+ * @byte: fixed-width 8-bit int to append to the end of the array.
+ * Return: void.
+*/
+void writeChunk(Chunk *chunk, uint8_t byte)
+{
+	if (chunk->capacity < chunk->count + 1)
+	{
+		int oldCapacity = chunk->capacity;
 
-    chunk->code[chunk->count] = byte;
-    chunk->count++;
-    
+		chunk->capacity = GROW_CAPACITY(oldCapacity);
+		chunk->code = GROW_ARRAY(
+			u_int8_t, chunk->code, oldCapacity, chunk->capacity);
+	}
+
+	chunk->code[chunk->count] = byte;
+	chunk->count++;
+
 }
