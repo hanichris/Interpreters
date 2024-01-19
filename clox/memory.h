b9c706add19c77c315832b776857c45e4543a6eb
hanichris <hanichris71@gmail.com>
2024-01-19 22:35:57 +0300

    define macros that support user-defined dynamic arrays in C.

--- clox/memory.h
+++ clox/memory.h
@@ -1,8 +1,15 @@
-#ifndef clox_memory.h
-#define clox_memory.h
+#ifndef clox_memory_h
+#define clox_memory_h
 
 #include "common.h"
 
+#define GROW_CAPACITY(capacity) \
+	((capacity) < 8 ? 8 : (capacity * 2))
 
+#define GROW_ARRAY(type, pointer, oldCount, newCount) \
+	(type *)reallocate(pointer, sizeof(type) * (oldCount), \
+	sizeof(type) * (newCount))
 
-#endif // clox_memory.h
+void *reallocate(void *pointer, size_t oldSize, size_t newSize);
+
+#endif
