4c55ea64ba5925693be15c0134f614ff470e124b
hanichris <hanichris71@gmail.com>
2024-01-19 22:32:47 +0300

    Add documentation to enum and structure in the header file.

--- clox/chunk.h
+++ clox/chunk.h
@@ -3,14 +3,27 @@
 
 #include "common.h"
 
-typedef enum {
-    OP_RETURN
+/**
+ * enum opcode - defines the various opcodes of the bytecode.
+ * @OP_RETURN: return instruction operation code.
+*/
+typedef enum opcode
+{
+	OP_RETURN
 } OpCode;
 
-typedef struct {
-    int count;
-    int capacity;
-    u_int8_t* code;
+
+/**
+ * struct ar - structure to define a dynamic array.
+ * @count: Number of entries currently in the array.
+ * @capacity: Current size of the dynamic array.
+ * @code: pointer to an array that will vary in size.
+*/
+typedef struct ar
+{
+	int count;
+	int capacity;
+	u_int8_t *code;
 } Chunk;
 
 
