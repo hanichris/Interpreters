9abf4c19b27669fd6dbe1388bceb629b73e1296e
hanichris <hanichris71@gmail.com>
2024-01-20 00:15:49 +0300

    Test functionality of newly created functions to ensure they work.

--- clox/main.c
+++ clox/main.c
@@ -1,5 +1,12 @@
 #include "common.h"
+#include "chunk.h"
 
 int main(int argc, char **argv) {
+	Chunk chunk;
+
+	initChunk(&chunk);
+	writeChunk(&chunk, OP_RETURN);
+	freeChunk(&chunk);
+	printf("%s\n", "Successfully reached the end of the program");
 	return (0);
 }
