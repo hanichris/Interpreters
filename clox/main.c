4552366b1e6a8b4634b386fca16ce2bee743a572
hanichris <hanichris71@gmail.com>
2024-01-28 00:50:35 +0300

    disassemble a chunk of memory in main to examine its contents.

--- clox/main.c
+++ clox/main.c
@@ -1,12 +1,15 @@
 #include "common.h"
 #include "chunk.h"
+#include "debug.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 	Chunk chunk;
 
 	initChunk(&chunk);
 	writeChunk(&chunk, OP_RETURN);
+	/* pass the chunk to the disassembler. */
+	disassembleChunk(&chunk, "Test chunk");
 	freeChunk(&chunk);
-	printf("%s\n", "Successfully reached the end of the program");
 	return (0);
 }
