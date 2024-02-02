52a6cbd03dbca3b1e3785eeb44a3bd8644e5a717
hanichris <hanichris71@gmail.com>
2024-02-03 00:05:29 +0300

    Define a structure to implement a dyamic array that will be the constant's list to keep track of constants that appear within the program.
    Provide utility functions similar to the chunk for writing a value to the dynamic array, freeing the array and initializing it.

---
+++ clox/value.h
@@ -0,0 +1,26 @@
+#ifndef clox_value_h
+#define clox_value_h
+
+#include "common.h"
+
+typedef double Value;
+
+/**
+ * struct valAr - structure that wraps a pointer to an array
+ * with its allocated capacity and number of elements in use.
+ * @capacity: The allocated size of the array.
+ * @count: The number of elements currently in the array.
+ * @values: Pointer to an array whose size is unknown at compile time.
+*/
+typedef struct valAr{
+    int capacity;
+    int count;
+    Value* values;
+} ValueArray;
+
+
+void initValueArray(ValueArray* array);
+void writeValueArray(ValueArray* array, Value value);
+void freeValueArray(ValueArray* array);
+
+#endif
