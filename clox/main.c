3b0df17a3d3d57bdb645e5d3154a93f5d263ddf4
hanichris <hanichris71@gmail.com>
2024-02-03 20:46:27 +0300

    Test out the OP_CONSTANT opcode instruction.

--- clox/main.c
+++ clox/main.c
@@ -7,6 +7,9 @@ int main(int argc, char **argv)
 	Chunk chunk;
 
 	initChunk(&chunk);
+	int constant = addConstant(&chunk, 1.2);
+	writeChunk(&chunk, OP_CONSTANT);
+	writeChunk(&chunk, constant);
 	writeChunk(&chunk, OP_RETURN);
 	/* pass the chunk to the disassembler. */
 	disassembleChunk(&chunk, "Test chunk");
