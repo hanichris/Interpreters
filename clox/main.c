295e6796228f5769d024425af57885886e27a17e
hanichris <hanichris71@gmail.com>
2024-02-05 00:41:48 +0300

    initialize the virtual machine and call the intrepret function to fire up the virtual machine on a chunk of bytecode. Finally, it frees up the memory of the virtual machine.

--- clox/main.c
+++ clox/main.c
@@ -1,10 +1,13 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, char **argv)
 {
 	Chunk chunk;
+	
+	initVM();
 
 	initChunk(&chunk);
 	int constant = addConstant(&chunk, 1.2);
@@ -12,7 +15,10 @@ int main(int argc, char **argv)
 	writeChunk(&chunk, constant, 123);
 	writeChunk(&chunk, OP_RETURN, 123);
 	/* pass the chunk to the disassembler. */
-	disassembleChunk(&chunk, "Test chunk");
+	// disassembleChunk(&chunk, "Test chunk");
+	/* command the VM to interpret a chunk of bytecode. */
+	interpret(&chunk);
+	freeVM();
 	freeChunk(&chunk);
 	return (0);
 }
