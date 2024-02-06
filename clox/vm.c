106861669c7baf71525c6ee49c08a4d36ea4ff9a
hanichris <hanichris71@gmail.com>
2024-02-06 16:30:15 +0300

    Implement stack protocol operations `pop` and `push`. Initialize the internal state of the VM by setting the pointer for the top of the stack to the beginning of the stack array.

--- clox/vm.c
+++ clox/vm.c
@@ -36,14 +36,55 @@ static InterpretResult run()
 	
 }
 
+/**
+ * resetStack - resets the stack by setting the pointer `stackTop`
+ * to point to the beginning of the array signifying an empty stack.
+*/
+static void resetStack()
+{
+	vm.stackTop = vm.stack;
+}
+
+/**
+ * initVM - initializes the internal state of the VM by setting the pointer
+ * of the top of the stack to the beginning of the stack array. There is no
+ * need to allocate space for the array as it is declared inline within the VM
+ * struct. Moreover, there is no need to clear unused cells as they simply won't
+ * be accessed until after values are stored within them.
+*/
 void initVM()
 {
-
+	resetStack();
 }
 
 void freeVM()
 {
 
+}
+
+/**
+ * push - pushes a new value on o the top of the stack. After which,
+ * it increments the `stackTop` pointer to point to the next unused
+ * slot in the array since the previous one now holds a value.
+ * @value: value to be placed onto the top of the stack.
+ * Return: void.
+*/
+void push(Value value)
+{
+	*vm.stackTop = value;
+	vm.stackTop++;
+}
+
+/**
+ * pop - retrieves the most recenty pushed value from the top of the stack.
+ * It first decrements the `stackTop` pointer to the value at the top of the
+ * stack before retrieving said value.
+ * Return: the value at the top of the stack.
+*/
+Value pop()
+{
+	vm.stackTop--;
+	return *vm.stackTop;
 }
 
 InterpretResult interpret(Chunk* chunk)
