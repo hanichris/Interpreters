5019b3bd929cec9ef24526c891a76f349f99f719
hanichris <hanichris71@gmail.com>
2024-02-06 19:29:33 +0300

    disassemble the OP_NEGATE opcode.

--- clox/debug.c
+++ clox/debug.c
@@ -73,15 +73,18 @@ int disassembleInstruction(Chunk *chunk, int offset)
 
 	switch (instruction)
 	{
-		case (OP_CONSTANT):
+		case OP_CONSTANT:
 			return constantInstruction("OP_CONSTANT", chunk, offset);
 
-		case (OP_RETURN):
+		case OP_NEGATE:
+			return simpleInstruction("OP_NEGATE", offset);
+
+		case OP_RETURN:
 			/* code */
-			return (simpleInstruction("OP_RETURN", offset));
+			return simpleInstruction("OP_RETURN", offset);
 
 		default:
 			printf("Unknown opcode %d\n", instruction);
-			return (offset + 1);
+			return offset + 1;
 	}
 }
