75e3e0431441a0ff557f6a678ce311ca22bcaf42
hanichris <hanichris71@gmail.com>
2024-02-12 22:21:44 +0300

    Reimplement the interpret function to serve as the driving function of the compiler.
    Include the compiler module.

--- clox/vm.c
+++ clox/vm.c
@@ -1,4 +1,5 @@
 #include "debug.h"
+#include "compiler.h"
 #include "vm.h"
 
 VM vm;
@@ -117,9 +118,8 @@ Value pop()
 	return *vm.stackTop;
 }
 
-InterpretResult interpret(Chunk* chunk)
+InterpretResult interpret(const char* source)
 {
-	vm.chunk = chunk;
-	vm.ip = vm.chunk->code;
-	return run();
+	compile(source);
+	return INTERPRET_OK;
 }
