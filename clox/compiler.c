4678245707e3f2961f1b7be5c7ed612eeef84eef
hanichris <hanichris71@gmail.com>
2024-02-19 02:57:55 +0300

    Define the parser table as an array.

--- clox/compiler.c
+++ clox/compiler.c
@@ -32,6 +32,21 @@ typedef enum _precedence
 	PREC_PRIMARY
 } Precedence;
 
+typedef void (*ParseFn)();
+
+/**
+ * struct _rule - defines a single row in the parser table.
+ * @prefix: function to compile a prefix expression.
+ * @infix: function to compile an infix expression.
+ * @precedence: precedence of an infix expression.
+*/
+typedef struct _rule
+{
+	ParseFn prefix;
+	ParseFn infix;
+	Precedence precedence;
+} ParseRule;
+
 Parser parser;
 Chunk* compilingChunk;
 
@@ -188,6 +203,28 @@ static void parsePrecedence(Precedence precedence)
 }
 
 /**
+ * binary - acts as the infix parser for the `TOKEN_PLUS`,
+ * `TOKEN_MINUS`, `TOKEN_STAR` and `TOKEN_SLASH` token types.
+ * It takes into consideration when parsing the right operand
+ * has a precedence level one higher than the binary operator.
+*/
+static void binary()
+{
+	TokenType operatorType = parser.previous.type;
+	ParseRule* rule = getRule(operatorType);
+	parsePrecedence((Precedence)rule->precedence + 1);
+
+	switch (operatorType)
+	{
+		case TOKEN_PLUS: emitByte(OP_ADD); break;		
+		case TOKEN_MINUS: emitByte(OP_SUBTRACT); break;
+		case TOKEN_STAR: emitByte(OP_MULTIPLY); break;
+		case TOKEN_SLASH: emitByte(OP_DIVIDE); break;
+		default: return;
+	}
+}
+
+/**
  * expression - parse the lowest precedence level and subsumes all
  * of the higher precedence expressions as well.
 */
@@ -241,6 +278,49 @@ static void unary()
 			return;
 	}
 }
+
+ParseRule rules[] = {
+	[TOKEN_LEFT_PAREN] 		= {grouping, NULL, PREC_NONE},
+	[TOKEN_RIGHT_PAREN] 	= {NULL, NULL, PREC_NONE},
+	[TOKEN_LEFT_BRACE] 		= {NULL, NULL, PREC_NONE},
+	[TOKEN_RIGHT_BRACE] 	= {NULL, NULL, PREC_NONE},
+	[TOKEN_COMMA] 			= {NULL, NULL, PREC_NONE},
+	[TOKEN_DOT] 			= {NULL, NULL, PREC_NONE},
+	[TOKEN_MINUS] 			= {unary, binary, PREC_TERM},
+	[TOKEN_PLUS] 			= {NULL, binary, PREC_TERM},
+	[TOKEN_SEMICOLON] 		= {NULL, NULL, PREC_NONE},
+	[TOKEN_SLASH] 			= {NULL, binary, PREC_FACTOR},
+	[TOKEN_STAR] 			= {NULL, binary, PREC_FACTOR},
+	[TOKEN_BANG] 			= {NULL, NULL, PREC_NONE},
+	[TOKEN_BANG_EQUAL] 		= {NULL, NULL, PREC_NONE},
+	[TOKEN_EQUAL] 			= {NULL, NULL, PREC_NONE},
+	[TOKEN_EQUAL_EQUAL] 	= {NULL, NULL, PREC_NONE},
+	[TOKEN_LESS] 			= {NULL, NULL, PREC_NONE},
+	[TOKEN_LESS_EQUAL] 		= {NULL, NULL, PREC_NONE},
+	[TOKEN_GREATER] 		= {NULL, NULL, PREC_NONE},
+	[TOKEN_GREATER_EQUAL] 	= {NULL, NULL, PREC_NONE},
+	[TOKEN_IDENTIFIER] 		= {NULL, NULL, PREC_NONE},
+	[TOKEN_STRING] 			= {NULL, NULL, PREC_NONE},
+	[TOKEN_NUMBER] 			= {number, NULL, PREC_NONE},
+	[TOKEN_AND] 			= {NULL, NULL, PREC_NONE},
+	[TOKEN_CLASS] 			= {NULL, NULL, PREC_NONE},
+	[TOKEN_ELSE] 			= {NULL, NULL, PREC_NONE},
+	[TOKEN_FALSE] 			= {NULL, NULL, PREC_NONE},
+	[TOKEN_FOR] 			= {NULL, NULL, PREC_NONE},
+	[TOKEN_FUN] 			= {NULL, NULL, PREC_NONE},
+	[TOKEN_IF] 				= {NULL, NULL, PREC_NONE},
+	[TOKEN_NIL] 			= {NULL, NULL, PREC_NONE},
+	[TOKEN_OR] 				= {NULL, NULL, PREC_NONE},
+	[TOKEN_PRINT] 			= {NULL, NULL, PREC_NONE},
+	[TOKEN_RETURN] 			= {NULL, NULL, PREC_NONE},
+	[TOKEN_SUPER] 			= {NULL, NULL, PREC_NONE},
+	[TOKEN_THIS] 			= {NULL, NULL, PREC_NONE},
+	[TOKEN_TRUE] 			= {NULL, NULL, PREC_NONE},
+	[TOKEN_VAR] 			= {NULL, NULL, PREC_NONE},
+	[TOKEN_WHILE] 			= {NULL, NULL, PREC_NONE},
+	[TOKEN_ERROR] 			= {NULL, NULL, PREC_NONE},
+	[TOKEN_EOF] 			= {NULL, NULL, PREC_NONE}
+};
 
 
 bool compile(const char* source, Chunk* chunk)
