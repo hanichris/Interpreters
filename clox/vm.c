5bd6f391fccb2e68536a74fa730b32ade67c55b5
hanichris <hanichris71@gmail.com>
2024-02-23 23:35:14 +0300

    Support reporting of runtime errors that occur during execution of the bytecode.
    Ensure the correct type is passed before performing binary operations.

--- clox/vm.c
+++ clox/vm.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdarg.h>
 
 #include "debug.h"
 #include "compiler.h"
@@ -6,15 +7,60 @@
 
 VM vm;
 
+/**
+ * peek - Gets a `Value` from the stack but does not pop it.
+ * @distance: How far down from the top of the stack to look - zero is
+ * the top, one is one slot down etc.
+ * Return: The `Value` at the given distance from the top.
+*/
+static Value peek(int distance)
+{
+	return vm.stackTop[-1 - distance];
+}
+
+/**
+ * resetStack - resets the stack by setting the pointer `stackTop`
+ * to point to the beginning of the array signifying an empty stack.
+*/
+static void resetStack()
+{
+	memset(vm.stack, 0, 256 * sizeof(Value));
+	vm.stackTop = vm.stack;
+}
+
+/**
+ * runtimeError - reports a useful error message to the user with the line
+ * of their code that was being executed when the error occurred.
+ * @format: the message string with a format layout.
+*/
+static void runtimeError(const char* format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	vfprintf(stderr, format, args);
+	va_end(args);
+	fputs("\n", stderr);
+
+	size_t instruction = vm.ip - vm.chunk->code - 1;
+	int line = vm.chunk->lines[instruction];
+	fprintf(stderr, "[line %d] in script\n", line);
+	resetStack();
+
+}
+
 static InterpretResult run()
 {
 	#define READ_BYTE() (*vm.ip++)
 	#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
-	#define BINARY_OP(op) \
+	#define BINARY_OP(valueType, op) \
 			do { \
-				double b = pop(); \
-				double a = pop(); \
-				push(a op b); \
+				if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { \
+					runtimeError("Operands must be numbers."); \
+					return INTERPRET_RUNTIME_ERROR; \
+				} \
+				double b = AS_NUMBER(pop()); \
+				double a = AS_NUMBER(pop()); \
+				push(valueType(a op b)); \
 			} while (false)
 			
 
@@ -43,15 +89,25 @@ static InterpretResult run()
 				break;
 			}
 
-			case OP_ADD: BINARY_OP(+); break;
-			case OP_SUBTRACT: BINARY_OP(-); break;
-			case OP_MULTIPLY: BINARY_OP(*); break;
-			case OP_DIVIDE: BINARY_OP(/); break;
+			case OP_FALSE: push(BOOL_VAL(false)); break;
+			case OP_TRUE: push(BOOL_VAL(true)); break;
+			case OP_NIL: push(NIL_VAL); break;
+
+			case OP_ADD: 		BINARY_OP(NUMBER_VAL, +); break;
+			case OP_SUBTRACT: 	BINARY_OP(NUMBER_VAL, -); break;
+			case OP_MULTIPLY: 	BINARY_OP(NUMBER_VAL, *); break;
+			case OP_DIVIDE: 	BINARY_OP(NUMBER_VAL, /); break;
 
 			case OP_NEGATE: {
+				if (!IS_NUMBER(peek(0)))
+				{
+					runtimeError("Operand must be a number");
+					return INTERPRET_RUNTIME_ERROR;
+				}
+				
 				// push(-pop()); break;
 				*(vm.stack + (int)(vm.stackTop - vm.stack) - 1) =
-					-*(vm.stack + (int)(vm.stackTop - vm.stack) - 1);
+					NUMBER_VAL(-AS_NUMBER(*(vm.stack + (int)(vm.stackTop - vm.stack) - 1)));
 				break;
 			}
 
@@ -67,16 +123,6 @@ static InterpretResult run()
 	#undef READ_CONSTANT
 	#undef READ_BYTE
 	
-}
-
-/**
- * resetStack - resets the stack by setting the pointer `stackTop`
- * to point to the beginning of the array signifying an empty stack.
-*/
-static void resetStack()
-{
-	memset(vm.stack, 0, 256 * sizeof(Value));
-	vm.stackTop = vm.stack;
 }
 
 /**
@@ -120,7 +166,7 @@ Value pop()
 	if (vm.stackTop == vm.stack)
 	{
 		fprintf(stderr, "Trying to pop from an empty stack\n");
-		return INTMAX_MIN;
+		return NIL_VAL;
 	}
 	vm.stackTop--;
 	return *vm.stackTop;
