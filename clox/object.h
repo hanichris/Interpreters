2bf08d3611f29727828253880ba7b6525e68c8f1
hanichris <hanichris71@gmail.com>
2024-02-25 01:18:04 +0300

    Define the struct for the string object type.
    Test if an object is of type string before trying to "downcast" the base Obj type to an ObjString type.

--- clox/object.h
+++ clox/object.h
@@ -5,16 +5,29 @@
 #include "value.h"
 
 #define OBJ_TYPE(value)			(AS_OBJ(value)->type)
+#define IS_STRING(value)		isObjType(value, OBJ_STRING)
 
 typedef enum
 {
-    OBJ_STRING,
+	OBJ_STRING,
 } ObjType;
 
 struct Obj
 {
-    ObjType type;
+	ObjType type;
 };
+
+struct ObjString
+{
+	Obj obj;
+	int length;
+	char* chars;
+};
+
+static inline bool isObjType(Value value, ObjType type)
+{
+	return IS_OBJ(value) && AS_OBJ(value)->type == type;
+}
 
 
 #endif // clox_object_h
