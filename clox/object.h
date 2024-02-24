05f3be607478d1e66545cd0d73e1e6c2a9914221
hanichris <hanichris71@gmail.com>
2024-02-25 00:49:49 +0300

    New module for the object type.

---
+++ clox/object.h
@@ -0,0 +1,20 @@
+#if !defined(clox_object_h)
+#define clox_object_h
+
+#include "common.h"
+#include "value.h"
+
+#define OBJ_TYPE(value)			(AS_OBJ(value)->type)
+
+typedef enum
+{
+    OBJ_STRING,
+} ObjType;
+
+struct Obj
+{
+    ObjType type;
+};
+
+
+#endif // clox_object_h
