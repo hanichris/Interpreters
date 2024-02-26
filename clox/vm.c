f55c0432509f9a4716edc646ec934efbba8dd456
hanichris <hanichris71@gmail.com>
2024-02-27 01:34:56 +0300

    Add functionality to concatenate strings together.

--- clox/vm.c
+++ clox/vm.c
@@ -3,6 +3,7 @@
 
 #include "debug.h"
 #include "compiler.h"
+#include "memory.h"
 #include "vm.h"
 
 VM vm;
@@ -21,6 +22,27 @@ static Value peek(int distance)
 static bool isFalsey(Value value)
 {
 	return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
+/**
+ * concatentate - joins together two string literals. Starts by calculating
+ * the length of the resultant string from the lengths of the two operands.
+ * Allocates a character array for the result and copies the two halves in.
+ * It finally properly terminates the string.
+*/
+static void concatenate()
+{
+	ObjString* b = AS_STRING(pop());
+	ObjString* a = AS_STRING(pop());
+
+	int length = a->length + b->length;
+	char* chars = ALLOCATE(char, length + 1);
+	memcpy(chars, a->chars, a->length);
+	memcpy(chars + a->length, b->chars, b->length);
+	chars[length] = '\0';
+
+	ObjString* result = takeString(chars, length);
+	push(OBJ_VAL(result));
 }
 
 /**
@@ -107,7 +129,23 @@ static InterpretResult run()
 			case OP_GREATER:	BINARY_OP(BOOL_VAL, >); break;
 			case OP_LESS:		BINARY_OP(BOOL_VAL, <); break;
 
-			case OP_ADD: 		BINARY_OP(NUMBER_VAL, +); break;
+			case OP_ADD: {
+				if (IS_STRING(peek(0)) && IS_STRING(peek(1)))
+				{
+					concatenate();
+				} else if (IS_NUMBER(peek(0)) && IS_NUMBER(peek(1)))
+				{
+					double a = AS_NUMBER(pop());
+					double b = AS_NUMBER(pop());
+					push(NUMBER_VAL(a + b));
+				} else
+				{
+					
+					runtimeError("Operands must be two numbers or two strings");
+					return INTERPRET_RUNTIME_ERROR;
+				}
+				break;
+			}
 			case OP_SUBTRACT: 	BINARY_OP(NUMBER_VAL, -); break;
 			case OP_MULTIPLY: 	BINARY_OP(NUMBER_VAL, *); break;
 			case OP_DIVIDE: 	BINARY_OP(NUMBER_VAL, /); break;
