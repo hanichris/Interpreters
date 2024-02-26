0d1a954c6ac8891ddf77e05c351aa4f0bb41dfb9
hanichris <hanichris71@gmail.com>
2024-02-26 23:27:48 +0300

    Provide documentation to the user-defined types.
    Define macros to obtain the array of characters from an object type.

--- clox/object.h
+++ clox/object.h
@@ -7,16 +7,35 @@
 #define OBJ_TYPE(value)			(AS_OBJ(value)->type)
 #define IS_STRING(value)		isObjType(value, OBJ_STRING)
 
-typedef enum
+#define AS_STRING(value)		((ObjString*)AS_OBJ(value))
+#define AS_CSTRING(value)		(((ObjString*)AS_OBJ(value))->chars)
+
+/**
+ * enum _obj_type - defines the supported object types.
+*/
+typedef enum _obj_type
 {
 	OBJ_STRING,
 } ObjType;
 
+/**
+ * struct Obj - contains the state shared across all object
+ * types. Acts like a 'base class' for objects.
+ * @type: the `type` tag of the object.
+*/
 struct Obj
 {
 	ObjType type;
 };
 
+/**
+ * struct ObjString - defines the payload for `string` object types.
+ * It contains an array of characters stored in a separate heap-allocated
+ * array, the number of bytes in the array and the state shared by all objects.
+ * @obj: common state shared by all `object` types.
+ * @length: number of bytes in the array of characters (the length of the array).
+ * @chars: pointer to the heap-allocated array.
+*/
 struct ObjString
 {
 	Obj obj;
