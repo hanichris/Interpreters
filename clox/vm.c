bf994f6810a7418376151701122793777b35fcef
hanichris <hanichris71@gmail.com>
2024-02-29 22:21:42 +0300

    Operate on intermediate strings using the new flexible character array struct member.

--- clox/vm.c
+++ clox/vm.c
@@ -32,16 +32,10 @@ static bool isFalsey(Value value)
 */
 static void concatenate()
 {
-	ObjString* b = AS_STRING(pop());
-	ObjString* a = AS_STRING(pop());
+	ObjStringVec* b = AS_STRING(pop());
+	ObjStringVec* a = AS_STRING(pop());
 
-	int length = a->length + b->length;
-	char* chars = ALLOCATE(char, length + 1);
-	memcpy(chars, a->chars, a->length);
-	memcpy(chars + a->length, b->chars, b->length);
-	chars[length] = '\0';
-
-	ObjString* result = takeString(chars, length);
+	ObjStringVec* result = takeStringVec(a, b);
 	push(OBJ_VAL(result));
 }
 
