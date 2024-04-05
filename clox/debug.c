3d96c040c528fe9aba012459aa41485ab243fb19
hanichris <hanichris71@gmail.com>
2024-04-05 23:01:08 +0300

    disassemble the opcode instruction for global variables.

--- clox/debug.c
+++ clox/debug.c
@@ -114,6 +114,9 @@ int disassembleInstruction(Chunk *chunk, int offset)
 
 		case OP_POP:
 			return simpleInstruction("OP_POP", offset);
+		
+		case OP_DEFINE_GLOBAL:
+			return constantInstruction("OP_DEFINE_GLOBAL", chunk, offset);
 
 		case OP_PRINT:
 			return simpleInstruction("OP_PRINT", offset);
